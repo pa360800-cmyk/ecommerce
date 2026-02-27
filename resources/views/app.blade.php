80bd4a6eac8d22e09c6e4dd4eb840b8275fae3f0
pa360800-cmyk <pa360800@gmail.com>
2026-02-27 16:50:32 +0800

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,18 +5,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <title inertia>BSAB Agricultural Marketplace</title>
 
         <!-- Scripts -->
         @routes
+        @viteReactRefresh
         @vite(['resources/js/app.jsx'])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+
+    <body class="antialiased">
         @inertia
     </body>
 </html>
